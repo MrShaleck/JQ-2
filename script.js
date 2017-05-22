ec3a959c7d976c30a3fbf007eb569faa50756147
Marcin <marcinrszalek@gmail.com>
2017-05-22 20:20:38 +0200

    1comm

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ $(function(){
     //this code will execute after the DOM is loaded
     var carouselList = $("#carousel ul");
 
-    changeSlide()
+    function moveFirstSlide()
     {
-        carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide)
-    };
-    setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
-    setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
-    moveFirstSlide(){
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
         carouselList.css({marginLeft:0});
     };
+    function changeSlide()
+    {
+        carouselList.animate({"marginLeft": -400}, 500, moveFirstSlide)
+    };
+    setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
+    setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
+
 
 
 });
